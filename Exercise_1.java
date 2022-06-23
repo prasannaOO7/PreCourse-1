8ff33ea55737b39612798a5894e4d7d6c06a6e67
prasannaOO7 <70802340+prasannaOO7@users.noreply.github.com>
2022-06-23 15:10:03 -0400

    exercise 1 done

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,36 +1,59 @@
 class Stack { 
-    //Please read sample.java file before starting.
-  //Kindly include Time and Space complexity at top of each file
-    static final int MAX = 1000; 
-    int top; 
-    int a[] = new int[MAX]; // Maximum size of Stack 
-  
-    boolean isEmpty() 
-    { 
-        //Write your code here 
-    } 
+	  //Please read sample.java file before starting.
+	  //Kindly include Time and Space complexity at top of each file
+	    static final int MAX = 1000; 
+	    int top;
+	    int a[] = new int[MAX]; // Maximum size of Stack 
+	  
+	    boolean isEmpty() 
+	    { 
+	        //Write your code here
+	        return top==0;
+	    } 
 
-    Stack() 
-    { 
-        //Initialize your constructor 
-    } 
-  
-    boolean push(int x) 
-    { 
-        //Check for stack Overflow
-        //Write your code here
-    } 
-  
-    int pop() 
-    { 
-        //If empty return 0 and print " Stack Underflow"
-        //Write your code here
-    } 
-  
-    int peek() 
-    { 
-        //Write your code here
-    } 
+	    Stack() 
+	    { 
+	        //Initialize your constructor 
+	    	this.top=0;
+	    	this.a = new int[MAX];
+	    } 
+	  
+	    boolean push(int x) 
+	    {   if(top>(MAX-1)){
+	            return false;
+	        }
+	        //Check for stack Overflow
+	        //Write your code here
+	        a[top]=x;
+	        top++;
+	        return true;
+
+	    } 
+	  
+	    int pop() 
+	    { 
+	        //If empty return 0 and print " Stack Underflow"
+	        //Write your code here
+	        if(top==0){
+	            System.out.print("Stack Underflow");
+	            return 0;
+	        }
+	        else {
+	        int bb=a[top];
+	        top--;
+	        return a[bb];
+	        }
+	        
+	       
+	       
+
+	    } 
+	  
+	    int peek() 
+	    { 
+	        //Write your code here
+	        return a[top-1];
+	    }
 } 
   
 // Driver code 
